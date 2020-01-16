2d54ea5d5005054bd25c9aab75a5e60e60e7c096
Blake <blake.allred95@icloud.com>
2020-01-15 17:17:25 -0700

    edited my Test variables script

---
+++ Farmer V Aliens/Assets/Scripts/Assignments/Variables/Test.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+
+{
+    public int amount;
+    public float money;
+    public float cost; 
+    //Declaration
+    public int cookieBox = 20;
+   public float waterBottle;
+   private string firstName;
+   public double rbi;
+  
+    //type
+    GameObject player;
+    //Dclaration and initialization
+    
+
+
+    //Access modifier
+    public 
+        private
+   
+    
+
+    // Start is called before the first frame update
+    void Start()
+         
+    {
+        Cookies(amount, money, cost);
+        //function call
+        CheckScore();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        void Cookies(int amount, float money, float cost);
+
+    }
+}
