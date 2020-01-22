5ceace4f19b92d2d7ea4d3f388dfcd981e253ce8
Blake <blake.allred95@icloud.com>
2020-01-22 16:32:26 -0700

    Created new script moo

---
+++ Farmer V Aliens/Assets/Scripts/Assignments/Variables/Moo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Detect collision with another object
+    private void OnCollisionEnter(Collision other)
+    {
+        
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+}
