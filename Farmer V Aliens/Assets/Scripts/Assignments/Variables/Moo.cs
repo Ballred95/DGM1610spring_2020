af25f0e48f512b3030c00d78ecf67ee6bc396666
Blake <blake.allred95@icloud.com>
2020-01-24 00:18:12 -0700

    Changed velocity of transform

--- Farmer V Aliens/Assets/Scripts/Assignments/Variables/Moo.cs
+++ Farmer V Aliens/Assets/Scripts/Assignments/Variables/Moo.cs
@@ -3,20 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Moo : MonoBehaviour
+    
 {
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    
     // Update is called once per frame
     void Update()
     {
+        transform.Translate(0.2f, 0, 0);
 
-        transform.Translate(0,0.5f,0);
     }
 
+    
     //Detect collision with another object
     private void OnCollisionEnter(Collision other)
     {
