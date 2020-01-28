bcad98754f092fa3413d6eec5964fee7157c3094
Blake <blake.allred95@icloud.com>
2020-01-27 17:13:33 -0700

    Created trigger event and added debug messages for collisions

--- Farmer V Aliens/Assets/Scripts/Assignments/Variables/Moo.cs
+++ Farmer V Aliens/Assets/Scripts/Assignments/Variables/Moo.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Moo : MonoBehaviour
-    
+
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -18,16 +18,33 @@ public class Moo : MonoBehaviour
     {
         moveSpeed = .09f;
 
-        transform.Translate(moveSpeed*Input.GetAxis("Horizontal"), 0f, moveSpeed*Input.GetAxis("Vertical"));
-      
-        
+        transform.Translate(moveSpeed * Input.GetAxis("Horizontal"), 0f, moveSpeed * Input.GetAxis("Vertical"));
+
+
     }
 
     private void OnCollisionEnter(Collision other)
+
     {
-        Destroy(other.gameObject);
+
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            Debug.Log("Colliding with floow");
+        }
+
+        else if (other.gameObject.CompareTag("Obstacle"))
+
+        {
+            Debug.Log("Colliding with Obstacle");
+
+        }
+
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("You have entered the trigger zone");
+    }
 
 }
 
