941390cd34a7e810b42395f9ec26841b8a47de2c
Blake <blake.allred95@icloud.com>
2020-02-01 02:43:26 -0700

    Added folders and player movement

---
+++ Galaxyshooter/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed = 5.3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Take current position and assign as start position. 
+        transform.position = new Vector3(0, -3.38f, 0);
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float horizontalMouse = Input.GetAxis("Mouse X");
+        float verticalMouse = Input.GetAxis("Mouse Y");
+
+        transform.Translate(Vector3.right * speed * horizontalMouse * Time.deltaTime);
+        transform.Translate(Vector3.up * speed * verticalMouse * Time.deltaTime);
+
+    }
+}
