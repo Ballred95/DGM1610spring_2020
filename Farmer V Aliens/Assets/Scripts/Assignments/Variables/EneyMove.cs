fb8d9e9d1d088df395ca5f29634ab18757cc97ac
Blake <blake.allred95@icloud.com>
2020-02-12 17:19:42 -0700

    Added basic spawn and enemy mechanic

---
+++ Farmer V Aliens/Assets/Scripts/Assignments/Variables/EneyMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EneyMove : MonoBehaviour
+{
+
+    public Transform target;
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.LookAt(target);
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+
+    }
+}
