8370e3b64678e4babfe372db3e63785fd35372b4
Blake <blake.allred95@icloud.com>
2020-02-19 22:59:18 -0700

    Fixed lookat movement!

--- Farmer V Aliens/Assets/Scripts/Assignments/Variables/EneyMove.cs
+++ Farmer V Aliens/Assets/Scripts/Assignments/Variables/EneyMove.cs
@@ -4,22 +4,29 @@ using UnityEngine;
 
 public class EneyMove : MonoBehaviour
 {
-
-    public Transform target;
-    public float moveSpeed;
+    public GameObject player;
+    public Vector3 target;
+    public int moveSpeed;
+    public int damage;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //player = GameObject.Find("Player");
+      
+       
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        player = GameObject.Find("Player");
+        target = player.transform.position;
         transform.LookAt(target);
         transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
 
     }
+
+    
 }
