324a5c411d00b4fb48da2926dfb79e78fc2df050
Blake <blake.allred95@icloud.com>
2020-02-24 17:11:27 -0700

    Created addforce and velocity characteristics

--- Farmer V Aliens/Assets/Scripts/Assignments/Variables/EneyMove.cs
+++ Farmer V Aliens/Assets/Scripts/Assignments/Variables/EneyMove.cs
@@ -8,30 +8,33 @@ public class EneyMove : MonoBehaviour
     public Vector3 target;
     public int moveSpeed;
     public int damage;
+    private Rigidbody enemyRB;
 
     // Start is called before the first frame update
     void Start()
     {
-        //player = GameObject.Find("Player");
-      
        
+
+        enemyRB = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        player = GameObject.Find("Player");
-        target = player.transform.position;
-        transform.LookAt(target);
-        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+       // player = GameObject.Find("Player");
+       // target = player.transform.position;
+       // transform.LookAt(target);
+        //transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
 
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void FixedUpdate()
     {
-        if (collision.gameObject.CompareTag("Player"))
-            Debug.Log("took damager");
+        //Add Force
+        enemyRB.AddForce((player.transform.position - transform.position).normalized *moveSpeed);
     }
 
 }
