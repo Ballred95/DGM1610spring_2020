2913b2e71ca8890ea5b77627aa4defec7009994d
Blake <blake.allred95@icloud.com>
2020-03-09 15:49:01 -0600

    Started creating character controller jump mechanic

--- Farmer V Aliens/Assets/Scripts/Assignments/Variables/CapsuleCharacter.cs
+++ Farmer V Aliens/Assets/Scripts/Assignments/Variables/CapsuleCharacter.cs
@@ -6,7 +6,8 @@ public class CapsuleCharacter : MonoBehaviour
 {
     private CharacterController _controller;
     private float _speed = 20;
-    private float _graivty = 1.0f;
+    private float _graivty = 10.0f;
+    private float jumpHeight = 50f;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +21,7 @@ public class CapsuleCharacter : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         // define direction based on input 
-        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        Vector3 direction = new Vector3(horizontalInput, /*verticalInput,*/ 0, 0);
 
         //move based on that direction
         Vector3 velocity = direction * _speed;
@@ -28,18 +29,21 @@ public class CapsuleCharacter : MonoBehaviour
 
         _controller.Move(velocity * Time.deltaTime);
 
-        if (_controller.isGrounded == true)
+        if (_controller.isGrounded == true && Input.GetKeyDown(KeyCode.Space))
         {
-
+            velocity.y = jumpHeight;
 
         }
 
         else
         {
-            velocity.y -= 
+            velocity.y -= _graivty;
             
         }
 
-        
+        _controller.Move(velocity * Time.deltaTime);
+
+      
+
     }
 }
