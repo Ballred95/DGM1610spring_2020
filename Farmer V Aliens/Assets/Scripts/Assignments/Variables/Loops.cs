bf55cdfe1712c5d8df105e9732d67b6cb5f61551
Blake <blake.allred95@icloud.com>
2020-03-11 20:39:37 -0600

    Added basic for, foreach, dowhile loops

--- Farmer V Aliens/Assets/Scripts/Assignments/Variables/Loops.cs
+++ Farmer V Aliens/Assets/Scripts/Assignments/Variables/Loops.cs
@@ -8,6 +8,7 @@ public class Loops : MonoBehaviour
 {
 
     public int numEnemies = 5;
+    public int cupsInSink = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +19,23 @@ public class Loops : MonoBehaviour
             Debug.Log("Creating enemy number:" + i);
 
         }
+
+        while(cupsInSink > 0)
+
+        {
+
+            Debug.Log("I've wshed a cup");
+            cupsInSink--;
+
+        }
+
+        bool shouldContinue = false;
+        do
+        {
+
+            print("Hi World");
+        }
+        while (shouldContinue == true);
 
     }
 
