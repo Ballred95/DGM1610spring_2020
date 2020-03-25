d63252faef2e0ffdebfc59a43e8647faf1502451
Blake <blake.allred95@icloud.com>
2020-03-25 16:48:57 -0600

    Started enumerators

---
+++ Farmer V Aliens/Assets/Scripts/Assignments/Variables/Enum.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enum : MonoBehaviour
+{
+
+    enum Days {sun = 1, mon};
+
+    Days currentDay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentDay = Days.sun;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
