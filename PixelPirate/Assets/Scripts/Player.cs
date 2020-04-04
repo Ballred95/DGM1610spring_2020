38e33392fe726685dc04579e48591aeec64ef80e
Blake <blake.allred95@icloud.com>
2020-04-03 23:17:26 -0600

    Basic player movement added. subject to change

--- PixelPirate/Assets/Scripts/Player.cs
+++ PixelPirate/Assets/Scripts/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    public int speed = 5;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,40 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        PlayerMove();
+
+
+    }
+
+    void PlayerMove()
+    {
         
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+
+            transform.Translate(new Vector2(1, -1) * speed * Time.deltaTime);
+
+        }
+
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+
+            transform.Translate(new Vector2(-1, 1) * speed * Time.deltaTime);
+        }
+
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+        }
+
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector2(.5f, 0) * speed * Time.deltaTime);
+
+        }
+
     }
 }
