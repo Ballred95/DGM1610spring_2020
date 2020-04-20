8b245d2cb6b3de70cc332018db09b5c611293db5
Blake <blake.allred95@icloud.com>
2020-04-20 16:53:12 -0600

    Added playerhealth script, more on wanderer script

--- Farmer V Aliens/Assets/Scripts/Assignments/Variables/Wanderer.cs
+++ Farmer V Aliens/Assets/Scripts/Assignments/Variables/Wanderer.cs
@@ -11,11 +11,17 @@ public class Wanderer: MonoBehaviour
     private Transform target;
     private NavMeshAgent agent;
     private float timer;
+    private Vector3 heading;
+
 
     //detection
+    public float speed;
     public float alertDist;
     public float attackDist;
     private float distance;
+
+    //attack
+    public int damage;
 
 
     // Use this for initialization
@@ -25,9 +31,46 @@ public class Wanderer: MonoBehaviour
         timer = wanderTimer;
     }
 
+    private void Start()
+    {
+        distance = Vector3.Distance(target.position, transform.position);
+    }
+
     // Update is called once per frame
     void Update()
     {
+        distance = Vector3.Distance(target.position, transform.position);
+
+        //alert
+        if(distance < alertDist && distance > attackDist)
+        {
+            print("enemy see target");
+            speed += 2;
+            transform.LookAt(target);
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+
+        //following
+        else if(distance >= alertDist)
+        {
+            print("enemy follow");
+            heading = target.position - transform.position;
+            heading.y = 0;
+            speed += 5;
+            transform.LookAt(target);
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+            if(heading.magnitude <= attackDist)
+            {
+                print("enemy attacking!");
+                var health = target.gameObject.GetComponent<PlayerHealth>();
+            }
+
+
+
+        }
+
+        //wandering
         timer += Time.deltaTime;
 
         if (timer >= wanderTimer)
